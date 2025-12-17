f470bbeba8b2311e5ea012db05d719afb32cad0c
JulieDandrimont <90048560+JulieDandrimont@users.noreply.github.com>
2025-12-17 12:12:25 -0500

    test gitignore

---
+++ confi.js
@@ -0,0 +1,10 @@
+
+const config = {
+    db : {
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database : "craftdinner",// mamp ; user = root, password = root
+}};
+
+module.exports = config;
