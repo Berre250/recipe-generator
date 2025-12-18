3ed32fd95499d120cebb9f862756f1dc1fda1bed
donn-tkr <ousmanetkr223@gmail.com>
2025-12-19 00:38:06 +0100

    login update

--- routes/auth.js
+++ routes/auth.js
@@ -2,40 +2,30 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../services/auth");
 
-router.get("/{:username&}{:password}", async (req, res, next) => {
-  try {
-    console.log(req.query);
-    // const { username, password } = req.body;
+router.get("/login", async (req, res, next) => {
+  const { username, password } = req.query;
 
-    // // 1) Vérif body
-    // if (!username || !password) {
-    //   return res.status(400).json({
-    //     error: true,
-    //     message: "Missing username or password",
-    //   });
-    // }
-
-    // 2) Appel service
-    const user = await auth.login(req.query.username, req.query.password);
-
-    // // 3) Identifiants invalides
-    // if (!user) {
-    //   return res.status(401).json({
-    //     error: true,
-    //     message: "Invalid credentials",
-    //   });
-    // }
-
-    // // 4) OK
-    // return res.status(200).json({
-    //   success: true,
-    //   message: "login successful",
-    //   user,
-    // });
-  } catch (err) {
-    console.error(`Error while getting users `, err.message);
-    next(err);
+  if (!username || !password) {
+    return res.json({
+      success: false,
+      message: "Username ou mot de passe manquant",
+    });
   }
+
+  const user = await auth.login(username, password);
+
+  if (!user) {
+    return res.json({
+      success: false,
+      message: "Identifiants incorrects",
+    });
+  }
+
+  res.json({
+    success: true,
+    message: "Connexion réussie",
+    user,
+  });
 });
 
 module.exports = router;
